492e00c625a5c9c6dcad43e65b59af6af77aac2b
nigorora14 <niltongr@outlook.com>
2021-11-02 20:11:41 -0500

    .

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ server.listen(port,'0.0.0.0', function(){
 })
 
 app.get('/',(req, res) => {
-    res.send('ruta raiz del backend')
+    res.send('ruta raiz del backend, solo se vera con un TOKEN.')
 })
 //error handler
 app.use((err,req, res, next) => {
